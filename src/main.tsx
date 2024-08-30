133dee1d6e88464163da42cab207a2cd2de01aef
Valera <valerakaktotak@gmail.com>
2024-08-30 15:50:36 +0300

    added absolute path settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
+import App from '@/App.tsx'
+import '@/index.css'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import theme from '../theme.ts'
-import App from './App.tsx'
-import './index.css'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
